e05a0b63730d969f81849485ede270383fe31049
Wifft <90070891+Wifft@users.noreply.github.com>
2022-04-10 19:55:24 +0200

    Improved role persistence validation

--- app/Http/Controllers/MapsController.php
+++ app/Http/Controllers/MapsController.php
@@ -9,9 +9,14 @@ use Illuminate\Support\Facades\Log;
 
 use Illuminate\View\View;
 
+use App\Http\Clients\DiscordClient;
+
 use App\Models\DiscordUser;
 use App\Models\Map;
+use App\Models\UsersMap;
+
 use Illuminate\Support\Facades\Cookie;
+
 use Throwable;
 
 /**
@@ -29,6 +34,24 @@ class MapsController extends Controller
             $user = DiscordUser::with(['maps' => fn (BelongsToMany $query) : BelongsToMany => $query->wherePivot('map_id', $map->id)])
                 ->whereHas('maps', fn (Builder $query) : Builder => $query->where('id', $map->id))
                 ->find(Cookie::get('user_id'));
+            if (!is_null($user)) {
+                $client = new DiscordClient($user->token);
+                $hasRole = $client->hasServerRole($map->guild_id, $user->discord_id, $map->role_id);
+                $isAdmin = $client->hasServerRole($map->guild_id, $user->discord_id, $map->admin_role_id);
+
+                UsersMap::where(
+                    [
+                        ['map_id', $map->id],
+                        ['discord_user_id', $user->id]
+                    ]
+                )
+                    ->update(
+                        [
+                            'has_role' => $hasRole,
+                            'is_admin' => $isAdmin
+                        ]
+                    );
+            }
 
             session()->put('map_uuid', $uuid);
 
